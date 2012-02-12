be0f96be9b9a8b92d864c8d6628c07d088a1fdee
Sam Wong <sam.wong.1990@gmail.com>
2012-02-12 22:25:35 +0000

    more statusupdate

--- index.php
+++ index.php
@@ -31,102 +31,103 @@ $db = new PDO('sqlite:'.dirname(__FILE__) . '/wifi.db') or die("Can't open sqlit
 	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
 	<script type="text/javascript">
 
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-20974018-2']);
-	  _gaq.push(['_setDomainName', '.']);
-	  _gaq.push(['_trackPageview']);
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-20974018-2']);
+	_gaq.push(['_setDomainName', '.']);
+	_gaq.push(['_trackPageview']);
 
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
 
-	</script>
-</head>
+		</script>
+	</head>
 
-<div class="navbar navbar-fixed-top">
-	<div class="navbar-inner">
-		<div class="container">
-			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</a>
-			<a class="brand" href="#">BTHomeHub2 wifi Downtime</a>
-			<div class="nav-collapse">
-				<ul class="nav">
-					<li class=""><a href="https://github.com/samwong1990/bthomehub-reliability">Source Code</a></li>
-				</ul>
-			</div><!--/.nav-collapse -->
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				<a class="brand" href="#">BTHomeHub2 wifi Downtime</a>
+				<div class="nav-collapse">
+					<ul class="nav">
+						<li class=""><a href="https://github.com/samwong1990/bthomehub-reliability">Source Code</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
 		</div>
 	</div>
-</div>
 
-<div class="container">
-	<!-- Example row of columns -->
-	<div class="row" style="text-align:center;">
-		<div class="span4">
-			<h1>Latest Outage</h1>
-			<h3>
-			<?php
+	<div class="container">
+		<!-- Example row of columns -->
+		<div class="row" style="text-align:center;">
+			<div class="span4">
+				<h1>Latest Outage</h1>
+				<h3>
+					<?php
 				$results = $db->query("SELECT * FROM log ORDER BY start DESC LIMIT 1")->fetchAll(PDO::FETCH_ASSOC);
 				echo date("F j, Y, g:i a", intval($results[0]['start']));
-			?>
+				?>
 			</h3>
 			<p>It lasted for 
-			<?php $mins = round(intval($results[0]['duration'])/60.0);
-				if($mins < 60){
-					$mins = max(array(1, $mins));
-					echo "$mins mins";
-				}else{
-					$hours = round($mins/60);
-					echo "$hours hours";
-				}
+				<?php $mins = round(intval($results[0]['duration'])/60.0);
+			if($mins < 60){
+				$mins = max(array(1, $mins));
+				echo "$mins mins";
+			}else{
+				$hours = round($mins/60);
+				echo "$hours hours";
+			}
 			?> 
-			</p>
-		</div>
-		<div class="span4">
-			<h1>Resets performed</h1>
-			<h3>
+		</p>
+	</div>
+	<div class="span4">
+		<h1>Resets performed</h1>
+		<h3>
 			<?php
-				$results = $db->query("SELECT count(start) AS resets FROM log")->fetchAll(PDO::FETCH_ASSOC);
-				echo $results[0]['resets'];
-			?>
-			</h3>
-		</div>
-		<div class="span4">
-			<h1>Longest outage</h1>
-			<h3>
-				<?php
-				$results = $db->query("SELECT * FROM log ORDER BY duration DESC LIMIT 1")->fetchAll(PDO::FETCH_ASSOC);
-				$mins = round(intval($results[0]['duration'])/60.0);
-				if($mins < 60){
-					$mins = max(array(1, $mins));
-					echo "$mins mins";
-				}else{
-					$hours = round($mins/60);
-					echo "$hours hours";
-				}
+		$results = $db->query("SELECT count(start) AS resets FROM log")->fetchAll(PDO::FETCH_ASSOC);
+		echo $results[0]['resets'];
+		?>
+	</h3>
+</div>
+<div class="span4">
+	<h1>Longest outage</h1>
+	<h3>
+		<?php
+	$results = $db->query("SELECT * FROM log ORDER BY duration DESC LIMIT 1")->fetchAll(PDO::FETCH_ASSOC);
+	$mins = round(intval($results[0]['duration'])/60.0);
+	if($mins < 60){
+		$mins = max(array(1, $mins));
+		echo "$mins mins";
+	}else{
+		$hours = round($mins/60);
+		echo "$hours hours";
+	}
 
-				?>	
-			</h3>
-			<p>Happened at <?php echo date("F j, Y, g:i a", intval($results[0]['start']));?></p>
-		</div>
-	</div>
-	<!-- Main hero unit for a primary marketing message or call to action -->
-	<div class="hero-unit">
-		<h1 style="line-height:2em;">Downtime log</h1>
-		<p>I used to reboot my BTHomeHub2 once every few days, now I do it few times a day.</p>
-		<p>You want me to change the channel, so I did. To make sure I'm doing it right, I did a wifi survey right next to the router. Now I'm sure the router is <ol><li>Not next to the microwave</li><li>The wifi space is not crowded</li><li>No one is ±1 channel with me.</li></ol></p>
-		<p>It didn't do much, as you can see below. What else can I do to help you help me?</p>
-	</div>
+	?>	
+</h3>
+<p>Happened at <?php echo date("F j, Y, g:i a", intval($results[0]['start']));?></p>
+</div>
+</div>
+<!-- Main hero unit for a primary marketing message or call to action -->
+<div class="hero-unit">
+	<h1 style="line-height:2em;">Downtime log</h1>
+	<p>I used to reboot my BTHomeHub2 once every few days, now I do it few times a day.</p>
+	<p>You want me to change the channel, so I did. To make sure I'm doing it right, I did a <a href="#wifisurvey">wifi survey</a> right next to the router. Now I'm sure the router is <ol><li>Not next to the microwave</li><li>The wifi space is not crowded</li><li>No one is ±1 channel with me.</li></ol></p>
+	<p>It didn't do much, as you can see below. What else can I do to help you help me?</p>
+	<p>Update 11-Feb : No I can't stick with wired connection either. The wired network dies with the wireless network.</p>
+</div>
 
-	<section id="typography">
-	  <div class="page-header">
-	    <h1>List of all outages <small>Since 7 Feb 2012, 02:00.</small></h1>
-	  </div>
-	  <div class="row">
+<section id="typography">
+	<div class="page-header">
+		<h1>List of all outages <small>Since 7 Feb 2012, 02:00.</small></h1>
+	</div>
+	<div class="row">
 		<div class="span12">
 			<div class="span12" style="text-align:center">
 				<h5>In a way, the shorter the duration, the more we are affected.</h5>
@@ -134,36 +135,57 @@ $db = new PDO('sqlite:'.dirname(__FILE__) . '/wifi.db') or die("Can't open sqlit
 			</div>
 			<table class="table table-striped table-bordered">
 				<thead>
-					<th>Started at</th><th>Ended at</th><th>Duration (mins)</th>
+					<th>Started at</th><th>Ended at</th><th>Duration</th>
 				</thead>
 				<tbody>
 					<?php
-						$results = $db->query("SELECT * FROM log ORDER BY start DESC")->fetchAll(PDO::FETCH_ASSOC);
-						foreach($results as $result){
-							?>
-								<tr>
-									<td><?php echo date("F j, Y, g:i a", intval($result['start']));?></td>
-									<td><?php echo date("F j, Y, g:i a", intval($result['end']));?></td>
-									<td><?php echo max(
-										array(
-											1,
-											round(intval($result['duration'])/60.0)
-											)
-										);?>
-									</td>
-								</tr>
-							<?php
-						}
+				$results = $db->query("SELECT * FROM log ORDER BY start DESC")->fetchAll(PDO::FETCH_ASSOC);
+				foreach($results as $result){
 					?>
+					<tr>
+						<td><?php echo date("F j, Y, g:i a", intval($result['start']));?></td>
+						<td><?php echo date("F j, Y, g:i a", intval($result['end']));?></td>
+						<td><?php $mins = max(
+											array(
+												1,
+												round(intval($result['duration'])/60.0)
+												)
+											);
+											if($mins < 60){
+												echo "$mins mins";
+											}else{
+												$hours = round($mins/60.0);
+												echo "$hours hours";
+											}
+											
+											?>
+							</td>
+						</tr>
+						<?php
+				}
+				?>
 				</tbody>
 			</table>
 		</div>
 	</div>
-	<hr>
+</section>
 
-	<footer>
-		<p>&copy; Sam Wong 2012</p>
-	</footer>
+<section id="wifisurvey">
+	<div class="page-header">
+		<h1>Wifi survey <small>Right next to the router</small></h1>
+	</div>
+	<div class="row">
+		<div class="span12">
+			<img src="img/atRouter.jpg">
+		</div>
+	</div>
+</section>
+</div>
+<hr>
+
+<footer>
+	<p>&copy; Sam Wong 2012</p>
+</footer>
 
 </div> <!-- /container -->
 
